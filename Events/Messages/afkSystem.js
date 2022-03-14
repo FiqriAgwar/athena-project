0509af3d698cfa09ae59fe24f5347c0d34592475
Aliffiqri Agwar <fiqri.agwar@gmail.com>
2022-03-14 16:11:56 +0800

    add role mentioning in afk system

--- Events/Messages/afkSystem.js
+++ Events/Messages/afkSystem.js
@@ -17,23 +17,59 @@ module.exports = {
       UserId: message.author.id,
     });
 
-    if (message.mentions.members.size) {
-      const Embed = new MessageEmbed().setColor("RED");
+    let AFKList = [];
+    let Time = [];
+    let Status = [];
 
+    if (message.mentions.roles.size) {
+      message.mentions.roles.forEach((role) => {
+        if (role.members.size) {
+          role.members.forEach((member) => {
+            DB.findOne(
+              { GuildId: message.guild.id, UserId: member.id },
+              async (err, data) => {
+                if (err) throw err;
+                if (data) {
+                  if (!AFKList.includes(member)) {
+                    AFKList.push(member);
+                    Time.push(`<t:${data.Time}:R>`);
+                    Status.push(data.Status);
+                  }
+                }
+              }
+            );
+          });
+        }
+      });
+    }
+
+    if (message.mentions.members.size) {
       message.mentions.members.forEach((member) => {
         DB.findOne(
           { GuildId: message.guild.id, UserId: member.id },
           async (err, data) => {
             if (err) throw err;
             if (data) {
-              Embed.setDescription(
-                `${member} went AFK <t:${data.Time}:R>\n **Status**: ${data.Status}`
-              );
-              return message.reply({ embeds: [Embed], ephemeral: true });
+              if (!AFKList.includes(member)) {
+                AFKList.push(member);
+                Time.push(`<t:${data.Time}:R>`);
+                Status.push(data.Status);
+              }
             }
           }
         );
       });
+    }
+
+    if (AFKList.length > 0) {
+      const Embed = new MessageEmbed().setColor("RED");
+
+      Embed.setDescription(`One or more people in this list is going AFK.`);
+      Embed.addField(`Member: `, AFKList.join("\n"), true);
+      Embed.addField(`Since: `, Time.join("\n"), true);
+      Embed.addField(`Reason: `, Status.join("\n"), true);
+
+      return message.reply({ embeds: [Embed], ephemeral: true });
     }
   },
 };

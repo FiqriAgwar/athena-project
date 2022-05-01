ff36e9a35aef05a7f64ffbaa223798bae445ffb2
Aliffiqri Agwar <fiqri.agwar@gmail.com>
2022-05-02 01:01:37 +0800

    Add announcement setup for dev

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,6 +1,9 @@
-const { Client } = require("discord.js");
+const { Client, MessageEmbed } = require("discord.js");
 const mongoose = require("mongoose");
 const Database = process.env.DATABASE;
+const AnnDB = require("../../Structures/Schemas/ScheduledAnnounce");
+const Schedule = require("node-schedule");
+const { schedule } = require("node-cron");
 // const { Database } = require("../../Structures/config.json");
 
 module.exports = {
@@ -31,8 +34,41 @@ module.exports = {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       })
-      .then(() => {
+      .then(async () => {
         console.log("The client is now connected to database.");
+        const announcement = await AnnDB.find({
+          Schedule: { $gte: Date.now() },
+        });
+
+        announcement.forEach((ann) => {
+          try {
+            Schedule.scheduleJob(ann.Schedule, async () => {
+              client.guilds.cache.forEach((g) => {
+                if (!g.systemChannel) return;
+
+                g.systemChannel.send({
+                  embeds: [
+                    new MessageEmbed()
+                      .setAuthor({
+                        name: "Announcement from Pantheon",
+                        iconURL: client.user.displayAvatarURL({
+                          dynamic: true,
+                        }),
+                      })
+                      .setTitle(ann.Title)
+                      .setDescription(ann.Content),
+                  ],
+                });
+              });
+
+              ann.delete();
+            });
+
+            console.log("All announcement has been set.");
+          } catch (err) {
+            console.log(err);
+          }
+        });
       })
       .catch((err) => {
         console.log(err);
